6e987c671333c463b3e5a20ac8b0e8989ffcb7af
abhijeet <abhijeet.thakur1@gmail.com>
2022-02-23 16:34:11 +0530

    first commit

---
+++ src/main/java/com/vanguard/retail/allprivateequity/webservice/AllprivateequityApplication.java
@@ -0,0 +1,13 @@
+package com.vanguard.retail.allprivateequity.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllprivateequityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllprivateequityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanguard/retail/allprivateequity/webservice/AllprivateequityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.retail.allprivateequity.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllprivateequityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
